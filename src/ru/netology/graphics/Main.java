34871176b9bdcd4ab5373fb460a83ebfeb50e824
DmitriyAV <dima.alekceev@gmail.com>
2021-09-21 23:44:53 +0300

    new version

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -10,8 +10,8 @@ import java.io.PrintWriter;
 public class Main {
     public static void main(String[] args) throws Exception {
         TextGraphicsConverter converter = new Converter(); // Создайте тут объект вашего класса конвертера
-
-        /*GServer server = new GServer(converter); // Создаём объект сервера
+/*
+        GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем*/
 
         // Или то же, но с сохранением в файл:
--- src/ru/netology/graphics/image/TransformerIntToChar.java
+++ src/ru/netology/graphics/image/TransformerIntToChar.java
@@ -1,15 +1,13 @@
 package ru.netology.graphics.image;
 
+
 public class TransformerIntToChar implements TextColorSchema {
     char[] chars = {'#', '$', '@', '%', '*', '+', '-', '\''};
+    int period = 255 / chars.length;
 
     @Override
     public char convert(int color) {
-        char targetChar = ' ';
-        char[] currentChar = {(char) color};
-        for (int i = 0; i < currentChar.length; i++){
-           targetChar = currentChar[i / 8] = chars[i];
-        }
-        return targetChar;
+        int i =  Math.abs(color / period);
+        return chars[i];
     }
 }
